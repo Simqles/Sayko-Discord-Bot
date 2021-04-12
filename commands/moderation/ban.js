8bd4cc03ff6ab98d4ef90a5d818a15264e753bf8
Nedim Talha <79831688+Simqles@users.noreply.github.com>
2021-04-12 20:36:25 +0300

    ban, banlist and unban codes

---
+++ commands/moderation/ban.js
@@ -0,0 +1,51 @@
+const discord = require('discord.js');
+
+
+
+
+exports.run = async(client,message,args) => {
+    var guild = message.guild;
+    var banlayan = message.author.tag;
+    let banxx = await message.guild.fetchBans();
+    if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.reply('Kullanıcıyı Banlayamıyorum Çünkü `BAN_MEMBERS` Yetkim Yok.');
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(":no_entry: Bu komutu kullanabilmek için `BAN_MEMBERS` yetkisine sahip olmanız gerek.");
+   
+    var kisi = message.mentions.users.first() || client.users.resolve(args[0]) || client.users.cache.find(u => u.username === args[0]) || client.users.cache.get(args[0]);
+    if(!kisi) return message.reply("Banlayacağım Kişiyi Belirtmen Gerekiyor `ID / @mention / username`")
+   var sebeb = args.slice(1).join(" ");
+  
+  
+      if(message.author == kisi) return message.reply("Kendini Yasaklayamazsın!")
+      if (banxx.get(kisi.id)) return message.reply("Kişi Zaten Yasaklanmış!")
+  
+   var now = new Date()
+   if (!sebeb) {
+           try {
+            kisi.send(`${kisi} **${guild}** adlı sunucudan banlandınız.`)
+            message.channel.send(`**${kisi} banlandı.**`)
+            guild.members.ban(kisi, { reason: sebeb/*, days: gun*/});
+          } catch (error) {
+            message.reply("Bir Sorun Oldu Lütfen Botun Geliştiricisi veya Yapımcısıyla İletişime Geçiniz!")
+            console.log(error)
+          }
+   } else {
+   try {
+     kisi.send(`${kisi} **${guild}** adlı sunucudan banlandınız. \nNedeni: **${sebeb}**`)
+     message.channel.send(`**${kisi} banlandı. \nNedeni: ${sebeb}**`)
+     guild.members.ban(kisi, { reason: sebeb/*, days: gun*/});
+   } catch (error) {
+     message.reply("Bir Sorun Oldu Lütfen Botun Geliştiricisi veya Yapımcısıyla İletişime Geçiniz!")
+     console.log(error)
+   }
+}
+
+};
+exports.conf = {
+    aliases: ['yasakla', 'yasak'],
+    permLevel: 0,
+}
+exports.help = {
+    name: 'ban',
+    description: 'Kullanıcıyı yasaklar',
+    usage: '<prefix><name>'
+}
